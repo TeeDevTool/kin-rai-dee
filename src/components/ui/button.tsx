b694d70d7cd3388db24c5c71087d4afdcf19d372
TeeDevTool <91672405+TeeDevTool@users.noreply.github.com>
2025-02-09 00:53:39 +0700

    create randomizer

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,8 +17,8 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3 text-xs",
-        lg: "h-11 rounded-md px-8 w-full",
+        sm: "h-9 px-3 text-xs",
+        lg: "h-28 px-45 text-[2rem] [&_svg]:size-12",
         icon: "h-10 w-10",
       },
     },
